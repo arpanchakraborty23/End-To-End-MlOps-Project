3057f82548e5966b9b676415a9cff1849f84ccf4
ARPAN CHAKRABORTY <ARPANCHAKRABORTY500@GMAIL.COM>
2024-11-06 14:02:59 +0530

    data Transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.ds import logging
 from src.ds.pipeline.Data_Ingestion_Pipline import DataIngestionTrainPipline
 from src.ds.pipeline.Data_Validation_Pipline import DataValidationTrainPipline
+from src.ds.pipeline.Data_Transformation_Pipline import DataTransformationTrainPipline
 
 logging.info('Welcome to Data Science Project')
 
@@ -20,3 +21,11 @@ validation_obj=DataValidationTrainPipline()
 validation_obj.ValidationPipline()
 
 logging.info('************************* Data Validation Completed **************************')
+
+# Data Transformation
+logging.info('************************* Data Transformation **************************')
+
+transform_obj=DataTransformationTrainPipline()
+transform_obj.TransformationPipline()
+
+logging.info('************************* Data Transformation Completed **************************')
