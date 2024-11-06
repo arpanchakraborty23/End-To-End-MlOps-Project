01d0a3cb67f14b4ecd35befe8ece1f647df98477
ARPAN CHAKRABORTY <ARPANCHAKRABORTY500@GMAIL.COM>
2024-11-06 21:33:09 +0530

    prediction

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, jsonify, request, send_file
+
+from src.dsproject import logging as lg
+import os,sys
+
+
+from src.dsproject.pipeline.Prediction_pipline import PredictionPipeline
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to my application"
+
+
+@app.route("/train")
+def train_route():
+    try:
+        os.system('python main.py')
+
+        return "Training Completed."
+
+    except Exception as e:
+        raise e
+
+@app.route('/predict', methods=['POST', 'GET'])
+def upload():
+    
+    try:
+
+
+        if request.method == 'POST':
+            # it is a object of prediction pipeline
+            prediction_pipeline = PredictionPipeline(request)
+            
+            #now we are running this run pipeline method
+            prediction_file_detail = prediction_pipeline.run_pipeline()
+
+            lg.info("prediction completed. Downloading prediction file.")
+            return send_file(prediction_file_detail.prediction_file_path,
+                            download_name= prediction_file_detail.prediction_file_name,
+                            as_attachment= True)
+
+
+        else:
+            return render_template('upload_file.html')
+    except Exception as e:
+        raise e
+    
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug= True)
